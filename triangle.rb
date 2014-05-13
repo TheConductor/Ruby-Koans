6c296092ce919f08d2710c676a4acbd4dbcee285
Alex Aziz <alexaziz@nc.rr.com>
2014-05-13 13:08:34 -0400

    Completed tringles so that tests in about trinagles project files would pass

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+	if a==b and b==c
+		return :equilateral
+	end
+	if (a==b and b!=c) or (b==c and b!=a) or (a==c and b!=a)
+		return :isosceles
+	end	
+	if a!=b and a!=c and b!=c
+		return :scalene
+	end	
 end
 
 # Error class used in part 2.  No need to change this code.
