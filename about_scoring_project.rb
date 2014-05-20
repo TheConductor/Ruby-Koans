2f7224875cbf89bd391dad92fb288fc31d4dc113
Alex Aziz <alexhaziz@gmail.com>
2014-05-20 14:21:00 -0700

    Completed about_scoring_project
    Wrote the score method so that the game was scored properly

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -30,10 +30,72 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Your goal is to write the score method.
 
 def score(dice)
-  # You need to write this method
+	score = 0
+	numberOf1s = 0
+	numberOf2s = 0
+	numberOf3s = 0
+	numberOf4s = 0
+	numberOf5s = 0
+	numberOf6s = 0
+	
+	if dice != []
+		dice.each do |item|
+			if item == 1
+				numberOf1s=numberOf1s+1
+			end
+			if item == 2
+				numberOf2s=numberOf2s+1
+			end
+			if item == 3
+				numberOf3s=numberOf3s+1
+			end
+			if item == 4
+				numberOf4s=numberOf4s+1
+			end
+			if item == 5
+				numberOf5s=numberOf5s+1
+			end
+			if item == 6
+				numberOf6s=numberOf6s+1
+			end
+		end
+		
+		if numberOf1s >= 3
+			score += (numberOf1s/3)*1000
+			score += numberOf1s%3 *100
+		else
+			score += numberOf1s *100
+		end
+
+		if numberOf2s >= 3
+			score += (numberOf2s/3)*200
+		end
+	
+		if numberOf3s >= 3
+			score += (numberOf3s/3)*300
+		end
+	
+		if numberOf4s >= 3
+			score += (numberOf4s/3)*400
+		end
+
+		if numberOf5s >= 3
+			score += (numberOf5s/3)*500
+			score += numberOf5s%3 *50
+		else
+			score += numberOf5s * 50
+		end
+		
+		if numberOf6s >= 3
+			score += (numberOf6s/3)*600
+		end
+	
+	end	
+	return score
 end
 
 class AboutScoringProject < Neo::Koan
+
   def test_score_of_an_empty_list_is_zero
     assert_equal 0, score([])
   end
@@ -45,7 +107,7 @@ class AboutScoringProject < Neo::Koan
   def test_score_of_a_single_roll_of_1_is_100
     assert_equal 100, score([1])
   end
-
+  
   def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
     assert_equal 300, score([1,5,5,1])
   end
